0e004e09bd704528f9c64072035fc59f6f7d71c8
Brahm Prakash Mishra <bpmishra92@gmail.com>
2017-12-07 11:57:23 +0530

    Mergesort basics

--- 03_mergesort/tutorial.cpp
+++ 03_mergesort/tutorial.cpp
@@ -20,6 +20,8 @@ int main() {
 
 	mergeSort( array, 0, n-1 );
 
+	//Prints in reverse order
+
 	while ( array.size() > 0 ) {
 	
 		int output = array.back();
@@ -31,7 +33,43 @@ int main() {
 	return 0;
 }
 
-void merge( 
+void merge( vector<int> &array, int low, int middle, int high) {
+
+
+	int n1 = middle - low + 1; //size of smaller sub-array
+	int n2 = high - middle; 
+
+	int L[n1], R[n2];
+
+	int i, j, k;
+
+	//Copy sorted data into temp arrays
+	for ( i = 0; i < n1; i++ )
+		L[i] = array[low+i];
+	for ( j = 0; j < n2; j++ )
+		R[j] = array[middle+1+j];
+
+	//Merge temp arrays back into the main array
+	i = 0; j = 0; k = low;//Reset counters
+	
+	while ( i < n1 and j < n2 ) {
+
+		if ( L[i] <= R[j] ) 
+			array[k] = L[i++];
+
+		else
+			array[k] = R[j++];
+
+		k++;
+	}
+
+	while ( i< n1 )
+		array[k++] = L[i++];
+
+	while ( j < n2 )
+		array[k++] = R[j++];
+
+}
 
 void mergeSort( vector<int> &array, int low, int high) {
 
@@ -44,5 +82,7 @@ void mergeSort( vector<int> &array, int low, int high) {
 		mergeSort ( array, middle+1, high);
 
 	}
+
+	merge ( array, low, middle, high );
 
 }
