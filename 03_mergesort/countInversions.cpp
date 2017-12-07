1a50fae1af76b7a0d89981e73a1b4919eac83a8a
Brahm Prakash Mishra <bpmishra92@gmail.com>
2017-12-07 16:30:37 +0530

    Failed again, going to modfy code for mergeSort first

--- 03_mergesort/countInversions.cpp
+++ 03_mergesort/countInversions.cpp
@@ -1,57 +1,66 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-long countInversions(int[], int);
-long mergeSort ( int[], int, int);
-long merge( int[], int[],int, int);
+long countSplitInversions( int arr[], int leftLower, int middle, int rightUpper) {
+    long result = 0;
+    
+    int numLeft = middle - leftLower + 1;
+    int numRight = rightUpper - middle;
+    
+    int tempArray[numLeft + numRight], i = 0, k = 0;
+    
+    while ( i <= numLeft && i <= numRight) {
+        
+        if ( arr[i] <= arr [ middle + i + 1 ])
+            tempArray[k++] = arr[i++];
+        else {
+            tempArray[k++] = arr[ middle + i + 1 ];        
+            result += middle - i + 1;
+        }
+    }
+    
+    while ( i < numLeft )
+        tempArray[k++] = arr[i++];
+    while ( i < numRight )
+        tempArray[k++] = arr[i++];
+    
+    arr = tempArray;
+    
+    return result;
+    
+}
+
+long countInversions(int arr[], int leftLower, int rightUpper ) {
+    
+    if ( rightUpper <= leftLower )
+        return 0; //Done when array size <= 1
+    
+    else {
+        
+        int middle = leftLower + (rightUpper - leftLower) /2;
+        long result = countInversions ( arr, leftLower, middle);
+        result += countInversions ( arr, middle+1, rightUpper);
+        result += countSplitInversions( arr, leftLower, middle, rightUpper);
+        
+        return result;
+    }
+    
+}
 
 int main() {
-
-	int d; 
-	cin>>d;
-	
-	for ( int i = 0; i < d; i++ ) {
-
-		int n; 
-		cin >> n;
-
-		int arr[n];
-
-		for ( int j = 0; j < n; j++ )
-			cin>>arr[j];
-
-		long result = countInversions(arr, n-1);
-		
-		cout<< result << endl;
-		
-
-	}
-		
-	return 0;
+    int t;
+    cin >> t;
+    for(int a0 = 0; a0 < t; a0++){
+        int n;
+        cin >> n;
+        int arr[n];
+        for(int arr_i = 0; arr_i < n; arr_i++){
+           cin >> arr[arr_i];
+        }
+        long result = countInversions(arr, 0, n-1 );
+        cout << result << endl;
+    }
+    return 0;
 }
 
-long countInversions( int arr[], int high) {
-
-	
-	if ( high > 0) 
-		
-		long result = mergeSort( arr, 0, high);
-
-}
-
-long mergeSort( int arr[], int low, int high) {
-	
-	if ( high > low ) {
-	
-		int middle = ( low + high )/2; 
-		long cntInv1 = mergeSort ( arr, low, middle);
-		long cntInv2 = mergeSort ( arr, middle+1, high); 
-
-		return ( cntInv1 + cntInv2 + merge(  
-
-	}
-	
-}
